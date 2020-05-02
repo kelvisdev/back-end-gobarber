413a1c24a955308dc45a2806245c8ee05b082f9a
Kelvis Borges <kelvisbcc@gmail.com>
2020-05-02 01:22:31 -0300

    Change field and create migration for relationship between user and appointments

---
+++ src/models/User.ts
@@ -0,0 +1,32 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+// KISS - Keep It Simple & Stupid
+
+@Entity('users')
+class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default User;
